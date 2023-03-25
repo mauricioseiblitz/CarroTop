744be615d2a43a055a1fbde1b47ba24b24e28781
Mauricio3 <mauricio-monteiro@hotmail.com.br>
2023-03-25 15:47:23 -0300

    Menu Burger

--- frontweb/src/components/NavBar/index.tsx
+++ frontweb/src/components/NavBar/index.tsx
@@ -1,6 +1,7 @@
-import 'bootstrap/js/src/collapse.js';
-
 import './styles.css';
+
+import '@popperjs/core';
+import 'bootstrap/js/src/collapse';
 
 const NavBar = () => {
     return (
@@ -9,10 +10,21 @@ const NavBar = () => {
                 <a href="link" className="nav-logo-text">
                     <h4>Carros Top</h4>
                 </a>
-                <div>
-                    <ul>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#carrotop-navbar"
+                    aria-controls="carrotop-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="carrotop-navbar">
+                    <ul className="navbar-nav offset-md-2 main-menu">
                         <li>
-                            <a href="link">HOME</a>
+                            <a href="link" className="active">HOME</a>
                         </li>
                         <li>
                             <a href="link">CATÁLOGO</a>
